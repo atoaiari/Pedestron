3c4469206c2386d1b81ccd1217d62ab9c1073816
godimarcovr <godimarcovr@gmail.com>
2021-09-06 16:34:00 +0200

    update

---
+++ additional_functions/__init__.py
@@ -0,0 +1,10 @@
+# mmcv/visualization/__init__.py
+
+from .color import Color, color_val
+from .image import imshow, imshow_bboxes, imshow_det_bboxes, orientation_imshow_det_bboxes
+from .optflow import flowshow, flow2rgb, make_color_wheel
+
+__all__ = [
+    'Color', 'color_val', 'imshow', 'imshow_bboxes', 'imshow_det_bboxes',
+    'flowshow', 'flow2rgb', 'make_color_wheel', 'orientation_imshow_det_bboxes'
+]
